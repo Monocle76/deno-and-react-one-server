8c9044bb65ed50c475d68b920d853bd9f6719651
Monocle76 <monocle76@proton.me>
2023-02-13 21:48:50 +0530

    init

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
+interface ApiResponse {
+  message: string
+}
+
+const app = new Application();
+
+const router = new Router();
+
+router
+  .get("/api/message", ({ response }) => {
+    response.body = <ApiResponse>{ message: "Hello, World!" }
+  })
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(oakCors())
+  .listen({ port: 80 })
